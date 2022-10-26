94dc9f55cc22ea0b76efcafaaf91093b3b8f4787
STiwari <71030367+Stiwari28@users.noreply.github.com>
2022-10-26 13:14:28 +0530

    Update

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -10,6 +10,18 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    double w = MediaQuery.of(context).size.width;
+    double h = MediaQuery.of(context).size.height;
+    return Scaffold(
+        body: Column(
+      children: [
+        Container(
+          width: w,
+          height: h * 0.3,
+          decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage("assets/login.png"))),
+        )
+      ],
+    ));
   }
 }
