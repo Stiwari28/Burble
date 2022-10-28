20541d4c6d25ea936f5f2a55152a2e5ee65a2897
STiwari <71030367+Stiwari28@users.noreply.github.com>
2022-10-28 12:19:18 +0530

    Update

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -1,3 +1,4 @@
+import 'package:burble/welcomepage.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -14,6 +15,59 @@ class _LoginPageState extends State<LoginPage> {
       decoration: BoxDecoration(
           image: DecorationImage(
               image: AssetImage('assets/login.png'), fit: BoxFit.cover)),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(children: [
+          Container(
+            padding: EdgeInsets.only(left: 30, top: 340),
+            child: Text(
+              'Easily Connect with Anyone',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 50, top: 500),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) => LoginPage()));
+              },
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.white),
+                  padding: MaterialStateProperty.all(const EdgeInsets.all(10)),
+                  elevation: MaterialStateProperty.all(15),
+                  minimumSize: MaterialStateProperty.all(const Size(250, 40)),
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  )),
+              child: Text(
+                'Log In',
+                style: TextStyle(
+                    color: Color.fromARGB(255, 10, 55, 93),
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 20, top: 50),
+            child: FloatingActionButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => WelcomePage()));
+              },
+              backgroundColor: Colors.white,
+              foregroundColor: Color.fromARGB(255, 10, 55, 93),
+              child: const Icon(Icons.arrow_back_rounded),
+            ),
+          ),
+        ]),
+      ),
     );
   }
 }
--- lib/signuppage.dart
+++ lib/signuppage.dart
@@ -1,4 +1,7 @@
+import 'package:burble/welcomePage.dart';
 import 'package:flutter/material.dart';
+
+import 'loginpage.dart';
 
 class SignUpPage extends StatefulWidget {
   SignUpPage({Key? key}) : super(key: key);
@@ -14,6 +17,59 @@ class _SignUpPageState extends State<SignUpPage> {
       decoration: BoxDecoration(
           image: DecorationImage(
               image: AssetImage('assets/signup.png'), fit: BoxFit.cover)),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(children: [
+          Container(
+            padding: EdgeInsets.only(left: 30, top: 340),
+            child: Text(
+              'Easily Connect with Anyone',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 50, top: 500),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) => LoginPage()));
+              },
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.white),
+                  padding: MaterialStateProperty.all(const EdgeInsets.all(10)),
+                  elevation: MaterialStateProperty.all(15),
+                  minimumSize: MaterialStateProperty.all(const Size(250, 40)),
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  )),
+              child: Text(
+                'Sign In',
+                style: TextStyle(
+                    color: Color.fromARGB(255, 10, 55, 93),
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 20, top: 50),
+            child: FloatingActionButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => WelcomePage()));
+              },
+              backgroundColor: Colors.white,
+              foregroundColor: Color.fromARGB(255, 10, 55, 93),
+              child: const Icon(Icons.arrow_back_rounded),
+            ),
+          ),
+        ]),
+      ),
     );
   }
 }
