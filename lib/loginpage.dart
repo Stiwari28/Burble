c4f935ad5c0dd3e37b1561c66951c32e8f3c709e
STiwari <71030367+Stiwari28@users.noreply.github.com>
2022-10-28 21:51:45 +0530

    update

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -1,3 +1,4 @@
+import 'package:burble/signuppage.dart';
 import 'package:burble/welcomepage.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +12,21 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   TextEditingController nameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+
+  void _clearTextField() {
+    // Clear everything in the text field
+    nameController.clear();
+    // Call setState to update the UI
+    setState(() {});
+  }
+
+  void _ShowPassword() {
+    // Clear everything in the text field
+    passwordController.clear();
+    // Call setState to update the UI
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -44,13 +60,26 @@ class _LoginPageState extends State<LoginPage> {
             ),
           ),
           Container(
-            margin: EdgeInsets.only(left: 30, top: 350),
+            margin: EdgeInsets.only(left: 10, top: 320),
             padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
             child: TextField(
               controller: nameController,
-              decoration: const InputDecoration(
+              onChanged: (value) {
+                // Call setState to update the UI
+                setState(() {});
+              },
+              decoration: InputDecoration(
                 contentPadding: EdgeInsets.fromLTRB(0, 5, 10, 5),
-                prefixIcon: Icon(Icons.person),
+                prefixIcon: Icon(
+                  Icons.person,
+                  color: Color.fromARGB(255, 10, 55, 93),
+                ),
+                suffixIcon: nameController.text.isEmpty
+                    ? null // Show nothing if the text field is empty
+                    : IconButton(
+                        icon: const Icon(Icons.clear),
+                        onPressed: _clearTextField,
+                      ),
                 enabledBorder: UnderlineInputBorder(
                   borderSide: BorderSide(
                     color: Color.fromARGB(255, 10, 55, 93),
@@ -62,18 +91,34 @@ class _LoginPageState extends State<LoginPage> {
                       width: 2, color: Color.fromARGB(255, 6, 38, 64)),
                 ),
                 labelText: 'User Name',
+                labelStyle: TextStyle(
+                  color: Color.fromARGB(255, 6, 38, 64),
+                ),
               ),
             ),
           ),
           Container(
-            margin: EdgeInsets.only(left: 30, top: 420),
+            margin: EdgeInsets.only(left: 10, top: 380),
             padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
             child: TextField(
               obscureText: true,
               controller: passwordController,
-              decoration: const InputDecoration(
+              onChanged: (value) {
+                // Call setState to update the UI
+                setState(() {});
+              },
+              decoration: InputDecoration(
                 contentPadding: EdgeInsets.fromLTRB(0, 5, 10, 5),
-                prefixIcon: Icon(Icons.password),
+                prefixIcon: Icon(
+                  Icons.lock,
+                  color: Color.fromARGB(255, 10, 55, 93),
+                ),
+                suffixIcon: passwordController.text.isEmpty
+                    ? null // Show nothing if the text field is empty
+                    : IconButton(
+                        icon: const Icon(Icons.panorama_fish_eye),
+                        onPressed: _ShowPassword,
+                      ),
                 enabledBorder: UnderlineInputBorder(
                   borderSide: BorderSide(
                     color: Color.fromARGB(255, 10, 55, 93),
@@ -85,22 +130,28 @@ class _LoginPageState extends State<LoginPage> {
                       width: 2, color: Color.fromARGB(255, 6, 38, 64)),
                 ),
                 labelText: 'Password',
+                labelStyle: TextStyle(
+                  color: Color.fromARGB(255, 6, 38, 64),
+                ),
               ),
             ),
           ),
           Container(
-            margin: EdgeInsets.only(left: 200, top: 490),
+            margin: EdgeInsets.only(left: 200, top: 440),
             child: TextButton(
               onPressed: () {
                 //forgot password screen
               },
               child: const Text(
                 'Forgot Password?',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 36, 98, 149),
+                ),
               ),
             ),
           ),
           Container(
-            margin: EdgeInsets.only(left: 50, top: 560),
+            margin: EdgeInsets.only(left: 50, top: 480),
             child: ElevatedButton(
               onPressed: () {
                 Navigator.of(context)
@@ -127,7 +178,7 @@ class _LoginPageState extends State<LoginPage> {
             ),
           ),
           Container(
-            margin: EdgeInsets.only(left: 70, top: 600),
+            margin: EdgeInsets.only(left: 70, top: 520),
             child: Text(
               'Dont have account ?',
               style: TextStyle(
@@ -136,10 +187,11 @@ class _LoginPageState extends State<LoginPage> {
             ),
           ),
           Container(
-            margin: EdgeInsets.only(left: 200, top: 596),
+            margin: EdgeInsets.only(left: 200, top: 516),
             child: TextButton(
               onPressed: () {
-                //forgot password screen
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => SignUpPage()));
               },
               child: const Text(
                 'Sign In',
--- lib/signuppage.dart
+++ lib/signuppage.dart
@@ -11,6 +11,34 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  //-------------------------------//
+  void _clearTextField() {
+    // Clear everything in the text field
+    nameController.clear();
+    // Call setState to update the UI
+    setState(() {});
+  }
+
+  //-----------------------------------//
+  void _clearEmailField() {
+    // Clear everything in the text field
+    emailController.clear();
+    // Call setState to update the UI
+    setState(() {});
+  }
+
+  //---------------------------------//
+  void _clearPhoneField() {
+    // Clear everything in the text field
+    phoneController.clear();
+    // Call setState to update the UI
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -31,32 +59,6 @@ class _SignUpPageState extends State<SignUpPage> {
             ),
           ),
           Container(
-            margin: EdgeInsets.only(left: 50, top: 500),
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) => LoginPage()));
-              },
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.white),
-                  padding: MaterialStateProperty.all(const EdgeInsets.all(10)),
-                  elevation: MaterialStateProperty.all(15),
-                  minimumSize: MaterialStateProperty.all(const Size(250, 40)),
-                  shape: MaterialStateProperty.all(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                  )),
-              child: Text(
-                'Sign In',
-                style: TextStyle(
-                    color: Color.fromARGB(255, 10, 55, 93),
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
-          ),
-          Container(
             margin: EdgeInsets.only(left: 20, top: 20),
             child: FloatingActionButton(
               mini: true,
@@ -69,6 +71,201 @@ class _SignUpPageState extends State<SignUpPage> {
               child: const Icon(Icons.arrow_back_rounded),
             ),
           ),
+          Container(
+            margin: EdgeInsets.only(left: 10, top: 290),
+            padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+            child: TextField(
+              controller: nameController,
+              onChanged: (value) {
+                // Call setState to update the UI
+                setState(() {});
+              },
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.fromLTRB(0, 5, 10, 5),
+                prefixIcon: Icon(
+                  Icons.person,
+                  color: Color.fromARGB(255, 10, 55, 93),
+                ),
+                suffixIcon: nameController.text.isEmpty
+                    ? null // Show nothing if the text field is empty
+                    : IconButton(
+                        icon: const Icon(Icons.clear),
+                        onPressed: _clearTextField,
+                      ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color.fromARGB(255, 10, 55, 93),
+                    width: 1.0,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                      width: 2, color: Color.fromARGB(255, 6, 38, 64)),
+                ),
+                labelText: 'Name',
+                labelStyle: TextStyle(
+                  color: Color.fromARGB(255, 6, 38, 64),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 10, top: 340),
+            padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+            child: TextField(
+              controller: emailController,
+              onChanged: (value) {
+                // Call setState to update the UI
+                setState(() {});
+              },
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.fromLTRB(0, 5, 10, 5),
+                prefixIcon: Icon(
+                  Icons.email,
+                  color: Color.fromARGB(255, 10, 55, 93),
+                ),
+                suffixIcon: emailController.text.isEmpty
+                    ? null // Show nothing if the text field is empty
+                    : IconButton(
+                        icon: const Icon(Icons.clear),
+                        onPressed: _clearEmailField,
+                      ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color.fromARGB(255, 10, 55, 93),
+                    width: 1.0,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                      width: 2, color: Color.fromARGB(255, 6, 38, 64)),
+                ),
+                labelText: 'Email',
+                labelStyle: TextStyle(
+                  color: Color.fromARGB(255, 6, 38, 64),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 10, top: 400),
+            padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+            child: TextField(
+              controller: phoneController,
+              onChanged: (value) {
+                // Call setState to update the UI
+                setState(() {});
+              },
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.fromLTRB(0, 5, 10, 5),
+                prefixIcon: Icon(
+                  Icons.phone,
+                  color: Color.fromARGB(255, 10, 55, 93),
+                ),
+                suffixIcon: phoneController.text.isEmpty
+                    ? null // Show nothing if the text field is empty
+                    : IconButton(
+                        icon: const Icon(Icons.clear),
+                        onPressed: _clearPhoneField,
+                      ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color.fromARGB(255, 10, 55, 93),
+                    width: 1.0,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                      width: 2, color: Color.fromARGB(255, 6, 38, 64)),
+                ),
+                labelText: 'Phone Number',
+                labelStyle: TextStyle(
+                  color: Color.fromARGB(255, 6, 38, 64),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 10, top: 460),
+            padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+            child: TextField(
+              obscureText: true,
+              controller: passwordController,
+              decoration: const InputDecoration(
+                contentPadding: EdgeInsets.fromLTRB(0, 5, 10, 5),
+                prefixIcon: Icon(
+                  Icons.lock,
+                  color: Color.fromARGB(255, 10, 55, 93),
+                ),
+                prefixIconColor: Color.fromARGB(255, 6, 38, 64),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color.fromARGB(255, 10, 55, 93),
+                    width: 1.0,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                      width: 2, color: Color.fromARGB(255, 6, 38, 64)),
+                ),
+                labelText: 'Create Password',
+                labelStyle: TextStyle(
+                  color: Color.fromARGB(255, 6, 38, 64),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 50, top: 550),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) => LoginPage()));
+              },
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(Color.fromARGB(255, 6, 38, 64)),
+                  padding: MaterialStateProperty.all(const EdgeInsets.all(10)),
+                  elevation: MaterialStateProperty.all(15),
+                  minimumSize: MaterialStateProperty.all(const Size(250, 40)),
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  )),
+              child: Text(
+                'Sign In',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 70, top: 600),
+            child: Text(
+              'Already have account ?',
+              style: TextStyle(
+                  color: Color.fromARGB(255, 36, 98, 149),
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 220, top: 596),
+            child: TextButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) => LoginPage()));
+              },
+              child: const Text(
+                'Log In',
+                style: TextStyle(
+                    color: Color.fromARGB(255, 6, 38, 64),
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          )
         ]),
       ),
     );
